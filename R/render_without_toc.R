e130041f4743e4f7d7127c561e411f4c05217630
Kate Isaac <41767733+kweav@users.noreply.github.com>
2025-08-04 13:38:55 -0400

    edit contents not path

--- R/render_without_toc.R
+++ R/render_without_toc.R
@@ -29,9 +29,6 @@ render_without_toc <- function(path = ".",
   # Find root directory by finding `.git` folder
   root_dir <- course_path()
 
-  # Add a file that specifies how links should be opened (prob should just add this file to the template instead of creating it here)
-  writeLines('<base target="_blank">', file.path(root_dir, "assets", "links.html"))
-
   # Output files:
   output_dir <- file.path(root_dir, output_dir)
 
@@ -52,8 +49,6 @@ render_without_toc <- function(path = ".",
     )
   }
   output_yaml_file <- file.path(root_dir, output_yaml)
-
-  output_yaml_file$`bookdown::gitbook`$includes$in_header <- c(output_yaml_file$`bookdown::gitbook`$includes$in_header, file.path(root_dir, "assets", "links.html"))
 
   # Make sure we have that file
   if (!file.exists(toc_close_css)) {
@@ -130,6 +125,13 @@ render_without_toc <- function(path = ".",
       overwrite = TRUE
     )
   }
+
+  # Add a file that specifies how links should be opened (prob should just add this file to the template instead of creating it here)
+  writeLines('<base target="_blank">', file.path(root_dir, "assets", "links.html"))
+
+  # Update yaml file before doing the rendering
+  output_yaml_lines$`bookdown::gitbook`$includes$in_header <- c(output_yaml_lines$`bookdown::gitbook`$includes$in_header, file.path(root_dir, "assets", "links.html"))
+  yaml::write_yaml(output_yaml_lines, output_yaml_file)
 
   ###### Now do the rendering! ######
   message("Render bookdown without TOC")
